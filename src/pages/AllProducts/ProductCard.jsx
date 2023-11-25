5245ecdc79a969e40d6f6224007a31ea2a6d4d4b
mdabarik <mdabarik19@gmail.com>
2023-11-25 22:54:29 +0600

    upvote functionality added"

--- src/pages/AllProducts/ProductCard.jsx
+++ src/pages/AllProducts/ProductCard.jsx
@@ -3,15 +3,68 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import Button from '@mui/material/Button';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Stack from '@mui/material/Stack';
+import useAuth from "../../hooks/useAuth";
+import useRole from "../../hooks/useRole";
+import { toast } from 'react-hot-toast';
 
-
-import { useState } from "react";
 
 const ProductCard = ({ product }) => {
+    const { user, loading } = useAuth();
+    // console.log(user, 'user prod');
+    // const [role] = useRole();
+    // console.log('role', role);
     const navigate = useNavigate();
+    const {
+        _id, prodName, prodImg, prodExtLink, prodTags, prodUpvotes, prodDownvotes, prodOwnerInfo
+    } = product;
+    const axiosPublic = useAxiosPublic();
 
-    const { _id, prodName, prodDesc, prodImg, prodExtLink, prodTags, prodStatus, prodUpvotes, prodDownvotes } = product;
+    const { data: votes, isLoading: loading1, refetch } = useQuery({
+        queryKey: ['upvotes', loading],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/votes?id=${_id}&email=${user?.email}`);
+            console.log(res, 'inside usequery prodcard');
+            return res.data;
+        }
+    })
 
+    const { data: voteCount, isLoading: loading2 } = useQuery({
+        queryKey: ['upvotescount', votes],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/count-votes/${_id}`);
+            console.log(res.data, 'vote count usequery prodcard');
+            return res.data;
+        }
+    })
+
+    console.log(voteCount, 'voteCount');
+
+
+    const handleUpvote = async () => {
+        // const prodOwnerEmail = prodOwnerInfo?.email;
+        // console.log(user?.id, 'inside handle upvote');
+        const prodId = _id;
+        const votes = {
+            userEmail: user?.email,
+            prodId: prodId,
+            types: 'upvote'
+        }
+        console.log(votes);
+        axiosPublic.post('/votes', votes)
+            .then(res => {
+                console.log(res);
+                if (res.data.insertedId) {
+                    toast.success("Upvoted succesfully");
+                    refetch()
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
 
     return (
         <div className="hover:cursor-pointer">
@@ -24,7 +77,7 @@ const ProductCard = ({ product }) => {
                     <p>Tags:</p>
                     <div className="flex gap-2">
                         {
-                            prodTags?.slice(0,5).map((tag, index) => <p key={index}>{tag}</p>)
+                            prodTags?.slice(0, 5).map((tag, index) => <p key={index}>{tag}</p>)
                         }
                     </div>
                 </div>
@@ -34,17 +87,52 @@ const ProductCard = ({ product }) => {
                     ({0} <span className="ml-2">reviews</span>)
                 </div>
                 <div className="flex justify-evenly mt-4">
-                    <div className="flex">
-                        <ArrowUpwardIcon />
-                        <p>(12)</p>
-                    </div>
-                    <div className="flex">
-                        <ArrowDownwardIcon />
-                        <p>(12)</p>
-                    </div>
+                    <Stack direction="row" spacing={2}>
+                        {
+                            // if prodowner === user: disable
+                            user && prodOwnerInfo?.email == user?.email || !user ?
+                                <Stack direction="row" spacing={2}>
+                                    <Button disabled size="small" sx={{ borderRadius: '10000px' }} variant="contained" startIcon={<ArrowUpwardIcon />}>
+                                        ({voteCount?.length})
+                                    </Button>
+                                    <Button disabled size="small" sx={{ borderRadius: '10000px' }} variant="outlined" startIcon={<ArrowDownwardIcon />}>
+                                        (0)
+                                    </Button>
+                                </Stack>
+                                :
+                                <Stack direction="row" spacing={2}>
+                                    {
+                                        // console.log(votes, 'invalid')
+                                        votes?.length > 0 ? <>
+                                            <Button disabled onClick={() => handleUpvote()} size="small" sx={{ borderRadius: '10000px' }} variant="contained" startIcon={<ArrowUpwardIcon />}>
+                                                ({voteCount?.length})
+                                            </Button></>
+                                            : <>
+                                                {
+                                                    !loading1 && !loading2 ? <>
+                                                        <Button onClick={() => handleUpvote()} size="small" sx={{ borderRadius: '10000px' }} variant="contained" startIcon={<ArrowUpwardIcon />}>
+                                                            ({voteCount?.length})
+                                                        </Button>
+                                                    </> : <>
+                                                        <span className="loading loading-spinner text-accent"></span>
+                                                    </>
+                                                }
+                                            </>
+
+                                    }
+                                    <Button size="small" sx={{ borderRadius: '10000px' }} variant="outlined" startIcon={<ArrowDownwardIcon />}>
+                                        (0)
+                                    </Button>
+                                </Stack>
+                        }
+
+                        {/* if user do not exist on click redirect to login page */}
+                    </Stack>
                 </div>
                 <div className="flex items-center justify-center mt-4">
-                    <Button onClick={() => navigate(`/all-products/${_id}`)} sx={{borderRadius: '0px',  padding: '8px', paddingX: '20px', width: '100%'}} startIcon={<VisibilityIcon />} variant="contained">View Details</Button>
+                    <Button
+                        onClick={() => navigate(`/all-products/${_id}`)}
+                        sx={{ borderRadius: '0px', padding: '8px', paddingX: '20px', width: '100%' }} startIcon={<VisibilityIcon />} variant="contained">View Details</Button>
                 </div>
             </div>
         </div>
