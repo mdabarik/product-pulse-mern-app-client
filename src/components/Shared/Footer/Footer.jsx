9b4da2b8bcea6f041525d2c6e7e717d87dfc4a1c
mdabarik <mdabarik19@gmail.com>
2023-11-29 05:03:59 +0600

    qr code added on the footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -6,6 +6,8 @@ import { BsFacebook } from "react-icons/bs";
 import useAuth from "../../../hooks/useAuth";
 import AOS from 'aos';
 import { useEffect } from "react";
+import QRCode from "react-qr-code";
+
 
 const Footer = () => {
     const { user } = useAuth();
@@ -75,9 +77,22 @@ const Footer = () => {
                 <p>USA, United States of America</p>
             </div>
 
+            <div className="mt-4">
+                <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+                    <QRCode
+                        size={256}
+                        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                        value={"https://product-pulse-web-app.web.app"}
+                        viewBox={`0 0 256 256`}
+                    />
+                </div>
+            </div>
+
             <div className="text-center border-t-2 mt-6">
                 <p className="mt-6 text-xs lg:text-sm">All Rights Reserved &copy; 2023-2029</p>
             </div>
+
+
         </footer>
     );
 };
